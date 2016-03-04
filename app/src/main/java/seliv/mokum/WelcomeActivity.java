04957f48b24f49aaa530ec22f5da548dea2a19c2
aselivanov <aseliv[]gmail>
2016-03-04 23:42:19 +0300

    Initial minimal project created.

---
+++ app/src/main/java/seliv/mokum/WelcomeActivity.java
@@ -0,0 +1,11 @@
+package seliv.mokum;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
