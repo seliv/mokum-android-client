f994266aec6bba74aef91680910e1cb4701493d3
aselivanov <aseliv[]gmail>
2016-03-05 01:44:32 +0300

    Welcome and Login activities added.

--- app/src/main/java/seliv/mokum/WelcomeActivity.java
+++ app/src/main/java/seliv/mokum/WelcomeActivity.java
@@ -1,11 +1,19 @@
 package seliv.mokum;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+    public void signIn(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
