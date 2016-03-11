3715b959a68994165b188cdb6a43f15616134a6c
aselivanov <aseliv[]gmail>
2016-03-11 22:53:23 +0300

    Link to token page added to login screen.

--- app/src/main/java/seliv/mokum/LoginActivity.java
+++ app/src/main/java/seliv/mokum/LoginActivity.java
@@ -3,8 +3,10 @@ package seliv.mokum;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,6 +14,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        TextView textView = (TextView) findViewById(R.id.TokenTextView);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     public void loginWithToken(View view) {
