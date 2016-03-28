fcd1cf1319b5954311c29ada5409f7edeee48d61
aselivanov <aseliv[]gmail>
2016-03-28 23:24:11 +0300

    Main menu with basic actions added.

--- app/src/main/java/seliv/mokum/MainActivity.java
+++ app/src/main/java/seliv/mokum/MainActivity.java
@@ -3,6 +3,9 @@ package seliv.mokum;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
@@ -122,6 +125,38 @@ public class MainActivity extends AppCompatActivity {
             super.onBackPressed();
         } else {
             goToUrl(visitedUrls.pop());
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_reload:
+                if (!visitedUrls.empty()) {
+                    String lastUrl = visitedUrls.pop();
+                    goToUrl(lastUrl);
+                    return true;
+                } else {
+                    return false;
+                }
+            case R.id.menu_home_feed:
+                goToUrl("index.json");
+                return true;
+//            case R.id.menu_my_feed:
+//                goToUrl("index.json");
+//                return true;
+            case R.id.menu_logout:
+                super.onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
